5f7e64b8b2300856e81ff61e6e8b5c6569f3f4de
Edgar Ernesto Campo Jaime <68762462+edgarcj@users.noreply.github.com>
2021-06-15 14:22:43 -0500

    Upload service worker
    Se sube archivo para funcionalidad de Service Worker

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "v1_cache_countdown",
+  urlsToCache = [
+    "./",
+    "./style.css",
+    "./script.js",
+    "./img/tiempo.png",
+    "./colinas.jpg",
+  ];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
+      })
+      .catch((err) => console.log("fallo el registro", err))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        });
+      })
+
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        return res;
+      }
+
+      return fetch(e.request);
+    })
+  );
+});
